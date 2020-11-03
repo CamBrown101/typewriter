f9854a01d44d331bf40900a6d662d0e606be0ca5
Cameron Brown <cameron-brown@live.com>
2020-11-03 20:22:16 +0000

    made a function that will print out each char in the same line, animated

---
+++ typewritter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+
+let delay = 0;
+let printString = ''
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+    printString += char
+    if (printString.length === sentence.length) {
+      console.log('')
+    }
+  }, delay)
+  delay += 50;
+}
